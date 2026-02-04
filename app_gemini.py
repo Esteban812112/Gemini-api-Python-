4edce7f41b597c227918a21ab9706db6060a3d58
Esteban81212 <magoelectrico1001@gmail.com>
2026-02-03 20:34:56 -0500

    Primera versión: conexión con Gemini

---
+++ app_gemini.py
@@ -0,0 +1,29 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+# 1. Cargar variables de entorno
+load_dotenv()
+clave_api = os.getenv("GEMINI_API_KEY")
+
+# 2. Inicializar cliente
+client = genai.Client(api_key=clave_api)
+
+def ejecutar_consulta():
+    print("🚀 Conectando con el motor de Gemini ...")
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview",
+            contents="Mani aqui tu  asistente de IA configurado para apoyar el curso de  IA'"
+        )
+
+        print("\n--- Respuesta Recibida ---")
+        print(response.text)
+        print("--------------------------")
+
+    except Exception as e:
+        print(f"❌ Ocurrió un error en la conexión: {e}")
+
+if __name__ == "__main__":
+    ejecutar_consulta()
